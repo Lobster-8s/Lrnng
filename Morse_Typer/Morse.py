c622d2f7ffd6ebee37a07b69f506ed7ea8f0bbdd
Lobster-8s <alex.ferrari.cod@gmail.com>
2026-04-30 18:11:19 +0200

    Added sounds to Morse.py | Must be fixed
    I've managed to make the sounds work, but wiht a few imperfections,to be fixed in a near future.

--- Morse_Typer/Morse.py
+++ Morse_Typer/Morse.py
@@ -1,3 +1,44 @@
+import numpy
+import pyaudio
+
+#The entire ContinuousWaves class was written by AI. I am not proud of myself.
+
+class ContinuousWaves:
+    """
+    MATH
+
+    signal(t) = volume⋅sin(2π⋅frequency⋅t)
+
+
+    """
+    def __init__(self):
+        self.frequency = 815
+        self.samplerate = 44100
+        self.volume = 0.8
+
+    def generate_tone(self, duration_ms):
+        num_samples = int(self.samplerate * duration_ms / 1000) #From ms to Samples   cus that's how u do it
+
+        t = numpy.arange(num_samples) / self.samplerate
+        return self.volume * numpy.sin(2 * numpy.pi * self.frequency * t)
+
+    def generate_silence(self, duration_ms):
+        num_samples = int(self.samplerate * duration_ms / 1000)
+        return numpy.zeros(num_samples)
+
+    def play(self, signal):
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=pyaudio.paFloat32,
+                        channels=1,
+                        rate=self.samplerate,
+                        output=True)
+
+        stream.write(signal.astype(numpy.float32).tobytes())
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
 
 def morse_to_letters():
     alphabet = {
@@ -48,22 +89,39 @@ def morse_to_letters():
         translated_phrase = []
 
         User_Input = input("Type 'list' fo a list of symbols and abbreviations\n>>  ")
-
         if User_Input == "list":
             print('R = Roger, Yes\nN = No, Negative\nDE = This is from [Code Name]\nEC = End of Transmission\nQRZ = "Who is calling me?"')
             continue
 
         words = User_Input.split("  ")
-
+        signal = []
+        Dot_Time = 100
+        #Split Words, then splits into letters
         for word in words:
             for letter in word.split(" "):
                 try:
                     translated_phrase.append(alphabet[letter])
+
                 except KeyError:
                     translated_phrase.append("[Unknown Value]")
+
+                for symbol in letter:
+                    if symbol == ".":
+                        signal.append(ContinuousWaves().generate_tone(Dot_Time))
+                    elif symbol == "-":
+                        signal.append(ContinuousWaves().generate_tone(Dot_Time * 3))
+                    elif symbol == " ":
+                        signal.append(ContinuousWaves().generate_silence(Dot_Time * 3))
+                    elif symbol == "/":
+                        signal.append(ContinuousWaves().generate_silence(Dot_Time * 7))
+                    #Gap between symbols
+                    signal.append(ContinuousWaves().generate_silence(Dot_Time))
+
             translated_phrase.append(" ")
 
+        final_signal = numpy.concatenate(signal)
         print("".join(translated_phrase))
+        ContinuousWaves().play(final_signal)
 
         if ". -.-." in User_Input: # if EC is in the user's text...
             print("-_-_-Transmission ended-_-_-")
